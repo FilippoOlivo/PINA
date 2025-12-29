8fbfde94af38e39164de7ea157c34f15d3de251b
FilippoOlivo <filippo@filippoolivo.com>
2025-12-29 15:22:11 +0100

    refact DomainEquationCondition

--- pina/condition/domain_equation_condition.py
+++ pina/condition/domain_equation_condition.py
@@ -1,12 +1,11 @@
 """Module for the DomainEquationCondition class."""
 
-from .condition_interface import ConditionInterface
-from ..utils import check_consistency
+from .condition_base import ConditionBase
 from ..domain import DomainInterface
 from ..equation.equation_interface import EquationInterface
 
 
-class DomainEquationCondition(ConditionInterface):
+class DomainEquationCondition(ConditionBase):
     """
     The class :class:`DomainEquationCondition` defines a condition based on a
     ``domain`` and an ``equation``. This condition is typically used in
@@ -30,7 +29,7 @@ class DomainEquationCondition(ConditionInterface):
     """
 
     # Available slots
-    __slots__ = ["domain", "equation"]
+    __fields__ = ["domain", "equation"]
 
     def __init__(self, domain, equation):
         """
@@ -41,24 +40,30 @@ class DomainEquationCondition(ConditionInterface):
         :param EquationInterface equation: The equation to be satisfied over the
             specified domain.
         """
+        if not isinstance(domain, (DomainInterface, str)):
+            raise ValueError(
+                f"`domain` must be an instance of DomainInterface, "
+                f"got {type(domain)} instead."
+            )
+        if not isinstance(equation, EquationInterface):
+            raise ValueError(
+                f"`equation` must be an instance of EquationInterface, "
+                f"got {type(equation)} instead."
+            )
         super().__init__()
         self.domain = domain
         self.equation = equation
 
-    def __setattr__(self, key, value):
-        """
-        Set the attribute value with type checking.
+    def __len__(self):
+        raise NotImplementedError(
+            "`__len__` method is not implemented for "
+            "`DomainEquationCondition` since the number of points is "
+            "determined by the domain sampling strategy."
+        )
 
-        :param str key: The attribute name.
-        :param any value: The value to set for the attribute.
-        """
-        if key == "domain":
-            check_consistency(value, (DomainInterface, str))
-            DomainEquationCondition.__dict__[key].__set__(self, value)
-
-        elif key == "equation":
-            check_consistency(value, (EquationInterface))
-            DomainEquationCondition.__dict__[key].__set__(self, value)
-
-        elif key in ("_problem"):
-            super().__setattr__(key, value)
+    def __getitem__(self, idx):
+        """ """
+        raise NotImplementedError(
+            "`__getitem__` method is not implemented for "
+            "`DomainEquationCondition`"
+        )
